8bef69719045f113d80232082a516b8a5a9b253d
Nicholas Petersen <nicholas.jay.petersen@gmail.com>
2025-09-29 21:00:16 -0400

    Create main and base class file

---
+++ Space.h
@@ -0,0 +1,6 @@
+
+class Space
+{
+	public:
+	protected:
+};
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Space.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	au = 1;
+	parsec = 1.5
+	lightyear = 2.4958;
+	holo = "string";
+	
+	push:"this is output";
+	holo = pull:"this is input";
+	
+	
+	return 0;
+}
