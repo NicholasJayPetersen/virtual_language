eb7a7cd956abf1c155e760708a2ff7f63f6cf1e8
Nicholas Petersen <nicholas.jay.petersen@gmail.com>
2025-10-03 21:17:51 -0400

    formatting

--- holocron.cpp
+++ holocron.cpp
@@ -1,7 +1,7 @@
 #include "holocron.h"
 
 holocron::holocron(const char* n) : s(n) {}		//chat GPT advised adding const because otherwise it it cannot bind string literals
-holocron& holocron::operator=(const char* n) { s = n; return *this; } //chatGPT advised chaging from a point return type to a reference for chaining purposes
+holocron& holocron::operator=(const char* n) {s = n; return *this;} //chatGPT advised chaging from a point return type to a reference for chaining purposes
 
 std::ostream& operator<<(std::ostream& output, holocron h)
 {
